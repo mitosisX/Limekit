8a9eed52880423eba036fa47472db6e3f0b8e181
Omega Msiska <35676707+mitosisX@users.noreply.github.com>
2023-12-20 14:17:47 +0200

    .

---
+++ limekit/framework/components/controls/widgets/gifplayer.py
@@ -0,0 +1,35 @@
+from PySide6.QtWidgets import QWidget, QLabel, QSizePolicy, QVBoxLayout
+from PySide6.QtGui import QMovie
+from PySide6.QtCore import QByteArray, Qt
+from limekit.framework.core.engine.parts import EnginePart
+
+
+class GifPlayer(QWidget, EnginePart):
+    def __init__(self, filename):
+        QWidget.__init__(self)
+
+        # Load the file into a QMovie
+        self.movie = QMovie(filename, QByteArray(), self)
+
+        size = self.movie.scaledSize()
+        # self.setGeometry(200, 200, size.width(), size.height())
+        self.resize(50, 50)
+
+        self.movie_screen = QLabel()
+        # Make label fit the gif
+        self.movie_screen.setSizePolicy(
+            QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed
+        )
+        self.movie_screen.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Create the layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.movie_screen)
+
+        self.setLayout(main_layout)
+
+        # Add the QMovie object to the label
+        self.movie.setCacheMode(QMovie.CacheMode.CacheAll)
+        self.movie.setSpeed(100)
+        self.movie_screen.setMovie(self.movie)
+        self.movie.start()
